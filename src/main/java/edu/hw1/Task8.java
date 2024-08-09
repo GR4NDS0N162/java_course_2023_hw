7222c961dbc768f960d27677c69a7a08e4c7bd29
Kirill Vnukov <kirillgrandson@gmail.com>
2024-08-10 01:10:57 +0300

    Сделал восьмое задание

---
+++ src/main/java/edu/hw1/Task8.java
@@ -0,0 +1,41 @@
+package edu.hw1;
+
+import org.jetbrains.annotations.NotNull;
+
+public class Task8 {
+    private static final int[][] OFFSETS = {
+        {2, 1},
+        {2, -1},
+        {-2, 1},
+        {-2, -1},
+        {1, 2},
+        {1, -2},
+        {-1, 2},
+        {-1, -2},
+    };
+
+    private Task8() {
+    }
+
+    public static boolean knightBoardCapture(int @NotNull [] @NotNull [] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == 1) {
+                    for (var offset : OFFSETS) {
+                        var x = j + offset[0];
+                        var y = i + offset[1];
+
+                        if (y < 0 || y >= board.length || x < 0 || x >= board[y].length) {
+                            continue;
+                        }
+
+                        if (board[y][x] == 1) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/edu/hw1/Task8Test.java
@@ -0,0 +1,73 @@
+package edu.hw1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Task8Test {
+    @Test
+    @DisplayName("test1")
+    void test1() {
+        // given
+        var board = new int[][] {
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 1, 0, 0, 0}
+        };
+
+        // when
+        var result = Task8.knightBoardCapture(board);
+
+        // then
+        assertThat(result).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("test2")
+    void test2() {
+        // given
+        var board = new int[][] {
+            {1, 0, 1, 0, 1, 0, 1, 0},
+            {0, 1, 0, 1, 0, 1, 0, 1},
+            {0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 1, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 1, 0, 1},
+            {1, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 1, 0, 1, 0, 1}
+        };
+
+        // when
+        var result = Task8.knightBoardCapture(board);
+
+        // then
+        assertThat(result).isEqualTo(false);
+    }
+
+    @Test
+    @DisplayName("test3")
+    void test3() {
+        // given
+        var board = new int[][] {
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0},
+            {1, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        // when
+        var result = Task8.knightBoardCapture(board);
+
+        // then
+        assertThat(result).isEqualTo(false);
+    }
+}
