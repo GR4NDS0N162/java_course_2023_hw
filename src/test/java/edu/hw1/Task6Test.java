256dc921c211749783c7a48eb2a5298fe1935e38
Kirill Vnukov <kirillgrandson@gmail.com>
2024-08-10 01:54:09 +0300

    Сделал шестое задание

--- src/test/java/edu/hw1/Task6Test.java
+++ src/test/java/edu/hw1/Task6Test.java
@@ -1,8 +1,8 @@
 package edu.hw1;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class Task6Test {
     @Test
