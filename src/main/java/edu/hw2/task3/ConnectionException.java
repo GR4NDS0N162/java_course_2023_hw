099fe8c6f23574b013e7f578118f64aac5b33f0d
Kirill Vnukov <kirillgrandson@gmail.com>
2024-08-11 19:04:38 +0300

    Удалил неиспользуемые конструкторы

--- src/main/java/edu/hw2/task3/ConnectionException.java
+++ src/main/java/edu/hw2/task3/ConnectionException.java
@@ -2,19 +2,11 @@ package edu.hw2.task3;
 
 public class ConnectionException extends RuntimeException {
 
-    public ConnectionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public ConnectionException(String message) {
-        super(message);
+    public ConnectionException() {
+        super();
     }
 
     public ConnectionException(Throwable cause) {
         super(cause);
-    }
-
-    public ConnectionException() {
-        super();
     }
 }
