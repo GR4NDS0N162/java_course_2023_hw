313a8a307602f8a75391dfe38ba691b7312d5dd6
Kirill Vnukov <kirillgrandson@gmail.com>
2024-08-11 14:19:41 +0300

    Добавил поддержку подстановки числа вместо выражения

--- src/main/java/edu/hw2/task1/Expr.java
+++ src/main/java/edu/hw2/task1/Expr.java
@@ -14,21 +14,49 @@ public sealed interface Expr {
 
     record Negate(Expr expr) implements Expr {
 
+        public Negate(double value) {
+            this(new Constant(value));
+        }
+
         @Override
         public double evaluate() {
             return -expr.evaluate();
         }
     }
 
-    record Exponent(Expr base, double pow) implements Expr {
+    record Exponent(Expr base, Expr pow) implements Expr {
+
+        public Exponent(double base, double pow) {
+            this(new Constant(base), new Constant(pow));
+        }
+
+        public Exponent(double base, Expr pow) {
+            this(new Constant(base), pow);
+        }
+
+        public Exponent(Expr base, double pow) {
+            this(base, new Constant(pow));
+        }
 
         @Override
         public double evaluate() {
-            return Math.pow(base.evaluate(), pow);
+            return Math.pow(base.evaluate(), pow.evaluate());
         }
     }
 
     record Addition(Expr l, Expr r) implements Expr {
+
+        public Addition(double l, double r) {
+            this(new Constant(l), new Constant(r));
+        }
+
+        public Addition(double l, Expr r) {
+            this(new Constant(l), r);
+        }
+
+        public Addition(Expr l, double r) {
+            this(l, new Constant(r));
+        }
 
         @Override
         public double evaluate() {
@@ -37,6 +65,18 @@ public sealed interface Expr {
     }
 
     record Multiplication(Expr l, Expr r) implements Expr {
+
+        public Multiplication(double l, double r) {
+            this(new Constant(l), new Constant(r));
+        }
+
+        public Multiplication(double l, Expr r) {
+            this(new Constant(l), r);
+        }
+
+        public Multiplication(Expr l, double r) {
+            this(l, new Constant(r));
+        }
 
         @Override
         public double evaluate() {
