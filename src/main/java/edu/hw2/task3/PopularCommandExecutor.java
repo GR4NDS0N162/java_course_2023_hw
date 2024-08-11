33950c7fb1ac729b1f97539e13dbbf68e712dc96
Kirill Vnukov <kirillgrandson@gmail.com>
2024-08-11 16:23:32 +0300

    Добавил исходные файлы для третьего задания

---
+++ src/main/java/edu/hw2/task3/PopularCommandExecutor.java
@@ -0,0 +1,20 @@
+package edu.hw2.task3;
+
+public class PopularCommandExecutor {
+
+    private final ConnectionManager manager;
+    private final int maxAttempts;
+
+    public PopularCommandExecutor(ConnectionManager manager, int maxAttempts) {
+        this.manager = manager;
+        this.maxAttempts = maxAttempts;
+    }
+
+    public void updatePackages() {
+        tryExecute("apt update && apt upgrade -y");
+    }
+
+    void tryExecute(String command) {
+        // TODO: Реализовать метод tryExecute(String command)
+    }
+}
