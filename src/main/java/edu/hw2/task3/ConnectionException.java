2162a578844fe7b531887e7502a99959f7af67fa
Kirill Vnukov <kirillgrandson@gmail.com>
2024-08-11 18:36:32 +0300

    Добавил конструкторы

--- src/main/java/edu/hw2/task3/ConnectionException.java
+++ src/main/java/edu/hw2/task3/ConnectionException.java
@@ -2,4 +2,19 @@ package edu.hw2.task3;
 
 public class ConnectionException extends RuntimeException {
 
+    public ConnectionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ConnectionException(String message) {
+        super(message);
+    }
+
+    public ConnectionException(Throwable cause) {
+        super(cause);
+    }
+
+    public ConnectionException() {
+        super();
+    }
 }
